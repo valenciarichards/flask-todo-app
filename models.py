e7650fcc225a2b1fbb37dff9f0df6aa9b176af14
Valencia Richards <63933204+valenciarichards@users.noreply.github.com>
2022-01-19 22:00:47 -0500

    Update models.py

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
 
+# Setup database configuration
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///book.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -11,21 +11,25 @@ db = SQLAlchemy(app)
 class User(db.Model):
     """User class to store users in the database"""
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
+    username = db.Column(db.String(30), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    # TODO add secure password field
+    password = db.Column(db.String(60))
+    tasks = db.relationship("Task", backref="user", lazy=True)
 
     def __repr__(self):
-        return self.username
+        return f"User: {self.username}, Email: {self.email}"
 
 
-class ToDO(db.Model):
-    """To-do class to store tasks in the database"""
+class Task(db.Model):
+    """To-Do class to store tasks in the database"""
     id = db.Column(db.Integer, primary_key=True)
-    task = db.Column(db.String(255), unique=False, nullable=False)
-    due_date = db.Column(db.DateTime, unique=False)
+    name = db.Column(db.String(255), unique=False, nullable=False)
+    due_date = db.Column(db.DateTime, unique=False, nullable=False)
+    # Priority will default to 4 (lowest) if none is entered.
+    priority = db.Column(db.Integer, unique=False, default=4)
+    # Mark tasks as incomplete by default.
     is_completed = db.Column(db.Boolean, unique=False, default=False)
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
 
     def __repr__(self):
-        return self.task
+        return f"Task: {self.name}, Due: {self.due_date}"
